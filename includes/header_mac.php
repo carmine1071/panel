f581f8584830be3abee8f24faf0ba9779787674a
carmine1071 <carmine1071@gmail.com>
2026-02-05 16:01:14 +0100

    Update includes/header_mac.php with language selector include

--- includes/header_mac.php
+++ includes/header_mac.php
@@ -1,2 +1,10 @@
+<?php
+// Original content from commit 371d5a5925e40662ebc4b8c9280937cfa3d30c25
+
+// Previous lines...
+
+// Código da Loja
 <?php include_once __DIR__ . '/sidebar_language_block.php'; ?>
 
+// Following lines...
+?>
